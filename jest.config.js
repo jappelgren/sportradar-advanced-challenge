2ba0c7e5f14be4caf2208a3dc86262ac50f57eca
jappelgren <justinappelgren@gmail.com>
2022-10-05 11:12:15 -0500

    added jest setup

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  roots: [
+    "src"
+  ],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node']
+};
